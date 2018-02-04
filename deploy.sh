f2c80a538054543855b84fa15b51f0c11f7b1a28
Phil Piwonka <philpiwonka@gmail.com>
2018-02-04 09:25:52 -0800

    improve deploy script to set cache-control headers on images

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,14 @@ export DISTRIBUTION=E2W4ET0T9WW0IT
 rm -rf public
 # rebuild site
 hugo
+
 # destructively sync with S3 bucket
 # (anything not mirrored on the local filesystem will be deleted)
-aws s3 sync public/ s3://$S3_BUCKET/ --delete
+# Excludes images
+aws s3 sync public/ s3://$S3_BUCKET/ --delete --exclude "images/*"
+
+# Sync images and set 'cache-control headers' for 1 year
+aws s3 sync public/ s3://$S3_BUCKET/ --delete --exclude "*" --include "images/*" --cache-control public,max-age=31536000
+
 # invalidate CloudFront
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION --paths /index.html /\*
