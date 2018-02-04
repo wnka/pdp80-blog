8edd85099367294fac9fe19beb0287c71293afc3
Phil Piwonka <philpiwonka@gmail.com>
2018-02-03 23:56:18 -0800

    Back to HTTPS, do invalidations right

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export S3_BUCKET=pdp80.com
+export DISTRIBUTION=E2W4ET0T9WW0IT
 # clean public build dir
 rm -rf public
 # rebuild site
@@ -7,3 +8,5 @@ hugo
 # destructively sync with S3 bucket
 # (anything not mirrored on the local filesystem will be deleted)
 aws s3 sync public/ s3://$S3_BUCKET/ --delete
+# invalidate CloudFront
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION --paths /index.html /\*
