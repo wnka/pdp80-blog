82dfe8328dd2e4d28fb39c2b879b2c9f5c79b3af
Phil Piwonka <wnka@users.noreply.github.com>
2024-04-10 19:50:08 -0700

    try again with the functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-export function onRequest(context) {
+export const onRequest: PagesFunction = async (context) => {
   const response = await context.next();
   console.log('got this context!' + context);
   return response;
