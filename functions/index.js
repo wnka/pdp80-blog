8767a97f8d95b379aeaaa3999fd6650ee9886c00
Phil Piwonka <wnka@users.noreply.github.com>
2024-04-10 19:51:39 -0700

    sigh try again!

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-export const onRequest: PagesFunction = async (context) => {
+export const onRequest = async (context) => {
   const response = await context.next();
   console.log('got this context!' + context);
   return response;
